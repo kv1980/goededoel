de95c23ae2733a56f5c073ce172a1a7de21a5c56
Kristof Vermeulen <kristofvermeulen1980@gmail.com>
2018-08-31 13:57:03 +0200

    begin javadoc (bij findbugs)

--- src/main/java/be/vdab/goededoel/entities/GoedeDoel.java
+++ src/main/java/be/vdab/goededoel/entities/GoedeDoel.java
@@ -2,26 +2,48 @@ package be.vdab.goededoel.entities;
 
 import java.math.BigDecimal;
 
+/**
+ * Een <strong>goed doel</strong> waarvoor men geld inzamelt
+ * @author curist
+ */
+
 public class GoedeDoel {
 	private final String naam;
 	private BigDecimal opgebracht = BigDecimal.ZERO;;
 
+	/**
+	 * maakt een GoedeDoel object aan
+	 * @param naam de naam van het goede doel
+	 */
 	public GoedeDoel(String naam) {
 		this.naam = naam;
 	}
 
+	/**
+	 * geeft de naam van dit doel terug
+	 * @return de naam
+	 */
 	public String getNaam() {
 		return naam;
 	}
 
+	/**
+	 * geeft terug hoeveel &euro; dit doel heeft opgebracht
+	 * @return het opgebrachte bedrag
+	 */
 	public BigDecimal getOpgebracht() {
 		return opgebracht;
 	}
-
+	
+	/**
+	 * verhoogt het opgebrachte bedrag
+	 * @param bedrag het bedrag, waarmee het opgebrachte bedrag wordt verhoogd
+	 */
 	public void verhoogOpgebracht(BigDecimal bedrag) {
 		opgebracht = opgebracht.add(bedrag);
 	}
 
+	@Override
 	public int hashCode() {
 		return this.naam.toUpperCase().hashCode();
 	}
---
+++ src/main/java/be/vdab/goededoel/entities/package-info.java
@@ -0,0 +1,4 @@
+/**
+* De classes in deze package stellen dingen in de werkelijkheid voor
+*/
+package be.vdab.goededoel.entities;
