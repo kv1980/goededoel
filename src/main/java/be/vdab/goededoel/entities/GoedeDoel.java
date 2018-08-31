4136b6567ca46b098b748f684098d740efeceb3e
Kristof Vermeulen <kristofvermeulen1980@gmail.com>
2018-08-31 10:57:22 +0200

    met JaCoCo

--- src/main/java/be/vdab/goededoel/entities/GoedeDoel.java
+++ src/main/java/be/vdab/goededoel/entities/GoedeDoel.java
@@ -5,17 +5,28 @@ import java.math.BigDecimal;
 public class GoedeDoel {
 	private final String naam;
 	private BigDecimal opgebracht = BigDecimal.ZERO;;
+
 	public GoedeDoel(String naam) {
 		this.naam = naam;
 	}
+
 	public String getNaam() {
 		return naam;
 	}
+
 	public BigDecimal getOpgebracht() {
 		return opgebracht;
 	}
-	
+
 	public void verhoogOpgebracht(BigDecimal bedrag) {
 		opgebracht = opgebracht.add(bedrag);
+	}
+
+	@Override
+	public boolean equals(Object object) {
+		if (!(object instanceof GoedeDoel)) {
+			return false;
+		}
+		return ((GoedeDoel) object).naam.equalsIgnoreCase(this.naam);
 	}
 }
